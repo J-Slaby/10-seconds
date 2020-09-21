1d59e9dd60a22f7afe72d114d1085040cbb5fe19
Ehan Rasgorshek <erasgorshek@gmail.com>
2020-09-20 20:34:20 -0500

    Imported placeholder sprite and began scripts for Input manager and movement.

---
+++ Assets/Scripts/Input/PlayerInputHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        
+    }
+    
+}
